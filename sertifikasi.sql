e043eb3b182769da7607c9fb9d992c11d1b52a2a
wediariasantana <wediariasantana@gmail.com>
2019-12-22 14:38:20 +0700

    tes

--- sertifikasi.sql
+++ sertifikasi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Dec 18, 2019 at 11:42 PM
--- Server version: 10.3.17-MariaDB-1
--- PHP Version: 7.2.9-1
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2019 at 08:34 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -37,15 +37,16 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'madara', 'ed06a360b820d6a9a2bcf218f53b9922');
+(1, 'madara', 'ed06a360b820d6a9a2bcf218f53b9922'),
+(2, 'ganteng', 'ganteng');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `datarSertifikasi`
+-- Table structure for table `datarsertifikasi`
 --
 
-CREATE TABLE `datarSertifikasi` (
+CREATE TABLE `datarsertifikasi` (
   `id_mhs` int(11) NOT NULL,
   `nim_mhs` int(11) NOT NULL,
   `nama_mhs` varchar(100) NOT NULL,
@@ -56,19 +57,29 @@ CREATE TABLE `datarSertifikasi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `datarSertifikasi`
+-- Dumping data for table `datarsertifikasi`
 --
 
-INSERT INTO `datarSertifikasi` (`id_mhs`, `nim_mhs`, `nama_mhs`, `prodi`, `sertifikasi`, `tgl_lahir`, `nohp`) VALUES
-(1, 165410089, 'naruto uzumaki', 'teknik informatika', 'CCNA', '1994-10-10', '087765345123');
+INSERT INTO `datarsertifikasi` (`id_mhs`, `nim_mhs`, `nama_mhs`, `prodi`, `sertifikasi`, `tgl_lahir`, `nohp`) VALUES
+(1, 165410089, 'naruto uzumaki', 'teknik informatika', 'CCNA', '1994-10-10', '087765345123'),
+(2, 1231212, 'aesrwer', 'ti', 'ccna', '2019-12-21', '123123123123'),
+(3, 1231212, 'aesrwer', 'ti', 'ccna', '2019-12-21', '123123123123'),
+(4, 165410029, 'WEDI ARIA SANTANA', 'ti', 'ccna', '2019-12-21', '085934885248'),
+(5, 16510022, 'tangguh', 'si', 'oracle', '2019-12-21', '123123123123'),
+(6, 16510022, 'tangguh', 'si', 'oracle', '2019-12-21', '123123123123'),
+(7, 16510022, 'tangguh', 'si', 'oracle', '2019-12-21', '123123123123'),
+(8, 16540212, 'antipecah', 'si', 'ccna', '2019-12-21', '123123123123'),
+(10, 165402121, 'tangguh22', 'ti', 'ccna', '2019-12-21', '123123123123'),
+(11, 165410022, 'gita', 'ti', 'toefl', '2019-12-21', '085934885222'),
+(14, 165410033, 'wahyu', 'TI', 'toefl', '2019-12-23', '085934885222');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `detailSertifikasi`
+-- Table structure for table `detailsertifikasi`
 --
 
-CREATE TABLE `detailSertifikasi` (
+CREATE TABLE `detailsertifikasi` (
   `id_detail` int(11) NOT NULL,
   `id_mhs` int(11) NOT NULL,
   `nim_mhs` int(11) NOT NULL,
@@ -83,10 +94,10 @@ CREATE TABLE `detailSertifikasi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `detailSertifikasi`
+-- Dumping data for table `detailsertifikasi`
 --
 
-INSERT INTO `detailSertifikasi` (`id_detail`, `id_mhs`, `nim_mhs`, `nama_mhs`, `prodi`, `sertifikasi`, `id_sertifikasi`, `dosen`, `tglMulai`, `tglSelesai`, `jam`) VALUES
+INSERT INTO `detailsertifikasi` (`id_detail`, `id_mhs`, `nim_mhs`, `nama_mhs`, `prodi`, `sertifikasi`, `id_sertifikasi`, `dosen`, `tglMulai`, `tglSelesai`, `jam`) VALUES
 (1, 1, 165410089, 'naruto uzumaki', 'teknik informatika', 'CCNA', 1, '', '2019-12-17', '2019-12-28', '10:00');
 
 -- --------------------------------------------------------
@@ -109,7 +120,11 @@ CREATE TABLE `sertifikasi` (
 --
 
 INSERT INTO `sertifikasi` (`id_sertifikasi`, `namaSertifikasi`, `tglMulai`, `tglSelesai`, `dosen`, `jam`) VALUES
-(1, 'CCNA', '2019-12-24', '2019-12-31', 'Wagito', '10:00');
+(1, 'CCNA', '2019-12-24', '2019-12-31', 'Wagito', '10:00'),
+(2, 'ccna', '2019-12-24', '2019-12-25', 'adiyuda', '12:04'),
+(3, 'ORACLE', '2019-12-24', '2019-12-27', 'AGUNG', '08:00'),
+(4, 'oracle', '2019-12-23', '2019-12-28', 'Bambang', '10:00'),
+(5, 'CCNA', '2019-12-30', '2020-01-01', 'WEDI', '08:30');
 
 --
 -- Indexes for dumped tables
@@ -122,15 +137,15 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `datarSertifikasi`
+-- Indexes for table `datarsertifikasi`
 --
-ALTER TABLE `datarSertifikasi`
+ALTER TABLE `datarsertifikasi`
   ADD PRIMARY KEY (`id_mhs`);
 
 --
--- Indexes for table `detailSertifikasi`
+-- Indexes for table `detailsertifikasi`
 --
-ALTER TABLE `detailSertifikasi`
+ALTER TABLE `detailsertifikasi`
   ADD PRIMARY KEY (`id_detail`),
   ADD KEY `id_mhs` (`id_mhs`),
   ADD KEY `id_sertifikasi` (`id_sertifikasi`);
@@ -149,31 +164,31 @@ ALTER TABLE `sertifikasi`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `datarSertifikasi`
+-- AUTO_INCREMENT for table `datarsertifikasi`
 --
-ALTER TABLE `datarSertifikasi`
-  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `datarsertifikasi`
+  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
--- AUTO_INCREMENT for table `detailSertifikasi`
+-- AUTO_INCREMENT for table `detailsertifikasi`
 --
-ALTER TABLE `detailSertifikasi`
+ALTER TABLE `detailsertifikasi`
   MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `sertifikasi`
 --
 ALTER TABLE `sertifikasi`
-  MODIFY `id_sertifikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_sertifikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `detailSertifikasi`
+-- Constraints for table `detailsertifikasi`
 --
-ALTER TABLE `detailSertifikasi`
-  ADD CONSTRAINT `detailSertifikasi_ibfk_1` FOREIGN KEY (`id_mhs`) REFERENCES `datarSertifikasi` (`id_mhs`),
+ALTER TABLE `detailsertifikasi`
+  ADD CONSTRAINT `detailSertifikasi_ibfk_1` FOREIGN KEY (`id_mhs`) REFERENCES `datarsertifikasi` (`id_mhs`),
   ADD CONSTRAINT `detailSertifikasi_ibfk_2` FOREIGN KEY (`id_sertifikasi`) REFERENCES `sertifikasi` (`id_sertifikasi`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
